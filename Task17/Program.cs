f8f5238fb81a59fdd5b05f734a498b0c26e46cc7
user <user@WIN-8I41D9CN0BC>
2023-01-02 13:23:58 +0300

    Добавьте файлы проекта.

---
+++ Task17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine(account1.Print());
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+            Console.WriteLine(account2.Print());
+            Console.ReadKey();
+        }
+    }
+}
